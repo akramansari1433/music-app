f631d127672254e6b6f2018d6bfed250865f751a
akramansari1433 <akramansari1433@gmail.com>
2023-07-07 01:19:32 +0530

    styles fix

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Sidebar() {
     return (
-        <div className="hidden lg:fixed lg:top-0 lg:bottom-20 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-            <div className="m-3 flex grow flex-col gap-y-5 overflow-y-auto rounded-xl bg-gray-800 px-6 py-4">
+        <div className="hidden lg:fixed lg:top-3 lg:bottom-24 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto rounded-xl bg-gray-800 px-6 py-4">
                 <h1 className="text-white text-4xl font-semibold">SideBar</h1>
             </div>
         </div>
