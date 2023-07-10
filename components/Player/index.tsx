2abe37f75fb26918649630f2e974cc69869a4a23
akramansari1433 <akramansari1433@gmail.com>
2023-07-10 22:59:52 +0530

    mobile ui fix

--- components/Player/index.tsx
+++ components/Player/index.tsx
@@ -42,33 +42,37 @@ export default function MusicPlayer() {
         >
             {activeSong && (
                 <div className="flex flex-row justify-between">
-                    <div className="hidden max-w-xs flex-row items-center gap-x-3 md:flex">
+                    <div className="max-w-xs flex-row items-center gap-x-3 md:flex">
                         <Image
-                            className="aspect-square rounded-md object-cover"
+                            className="h-16 w-full md:w-auto aspect-square rounded-md object-cover"
                             src={activeSong?.imageUrl}
                             alt="song-image"
                             width={50}
                             height={50}
                         />
 
-                        <div className="flex flex-col">
+                        <div className="hidden md:flex flex-col">
                             <span className="line-clamp-1">{activeSong?.name}</span>
                             <span className="line-clamp-1 text-xs">{activeSong && activeSong.artistName}</span>
                         </div>
                     </div>
-                    <div className="flex w-full flex-col gap-2 md:w-fit">
-                        <div className="flex flex-row items-center justify-center gap-5">
+
+                    {/* audio control */}
+                    <div className="w-[80%] md:w-auto flex flex-col gap-y-2">
+                        <div className="flex flex-row items-end justify-between md:justify-center gap-5">
+                            <span className="text-xs md:hidden">{formatDuration(currentTime)}</span>
                             <button
                                 className="rounded-full bg-black dark:bg-white p-2 text-white dark:text-black"
                                 onClick={() => dispatch(setPlaying(!isPlaying))}
                             >
                                 {isPlaying ? <PauseIcon className="h-6 w-6" /> : <PlayIcon className="h-6 w-6" />}
                             </button>
+                            <span className="text-xs md:hidden">{formatDuration(audioRef.current?.duration!)}</span>
                         </div>
-                        <div className="md:fit flex w-full flex-row items-center gap-3">
-                            <span className="text-xs">{formatDuration(currentTime)}</span>
+                        <div className="flex w-full flex-row items-center gap-3">
+                            <span className="hidden md:block text-xs">{formatDuration(currentTime)}</span>
                             <input
-                                className="w-full accent-slate-500 md:w-36"
+                                className="accent-slate-500 w-full md:w-60 lg:w-72"
                                 type="range"
                                 min={0}
                                 max={audioRef.current?.duration!}
@@ -78,10 +82,14 @@ export default function MusicPlayer() {
                                     audioRef.current!.currentTime = Number(e.target.value);
                                 }}
                             />
-                            <span className="text-xs">{formatDuration(audioRef.current?.duration!)}</span>
+                            <span className="hidden md:block text-xs">
+                                {formatDuration(audioRef.current?.duration!)}
+                            </span>
                         </div>
                     </div>
-                    <div className="hidden h-full w-64 flex-row items-center gap-3 md:flex">
+
+                    {/* volume slider */}
+                    <div className="hidden md:flex h-full w-auto flex-row items-center gap-3">
                         <button
                             id="volume-mute-unmute"
                             aria-label="Volume mute/unmute"
